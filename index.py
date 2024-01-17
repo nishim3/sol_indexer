5aa17457b80d99cb78dd1fc13f22e145a2ca2f2d
Nishim Goyal <nishimgoyal@Nishim.local>
2024-01-17 16:20:47 +0530

    counting restarts

--- index.py
+++ index.py
@@ -5,17 +5,23 @@ from pymongo import MongoClient
 url = 'https://solana-mainnet.g.alchemy.com/v2/CGiyOMQ5iorRtcPm2xnU7qOSkocN2Ps7'
 headers = {'Content-Type': 'application/json'}
 
-# Connect to MongoDB 
+# Connect to MongoDB
 client = MongoClient('localhost', 27017)
 db = client['solana_data']
 collection = db['blockchain_data']
 
-initial_slot_file = "slot_number.txt"
+api_file = "api_data.txt"
 
+# Initialize slot number and restart count
+slot_number = 0
+restart_count = 0
 
-if os.path.exists(initial_slot_file):
-    with open(initial_slot_file, "r") as file:
-        slot_number = int(file.read())
+# Check if the api file exists
+if os.path.exists(api_file):
+    with open(api_file, "r") as file:
+        data = file.read().split(',')
+        if len(data) == 2:
+            slot_number, restart_count = map(int, data)
 else:
     slot_data = {
     "jsonrpc": "2.0",
@@ -25,9 +31,9 @@ else:
     slot_response = requests.post(url, json=slot_data, headers=headers)
     slot_number = slot_response.json()['result']
 
+
 try:
     while True:
-
         print(f"Indexing block: {slot_number}")
 
         # Fetch the block for the current slot
@@ -51,7 +57,6 @@ try:
 
         for transaction in response_json['result']['transactions']:
             transaction['block'] = slot_number
-
             collection.insert_one(transaction)
 
         slot_number += 1
@@ -60,7 +65,11 @@ except Exception as e:
     print(f"An error occurred: {e}")
 
 finally:
-    with open(initial_slot_file, "w") as file:
-        file.write(str(slot_number))
+    # Increment restart count
+    restart_count += 1
+
+    # Update api file with slot number and restart count
+    with open(api_file, "w") as file:
+        file.write(f"{slot_number},{restart_count}")
 
     client.close()
