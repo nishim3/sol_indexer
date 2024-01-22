d6909752a81899b267de5a749729e2181e3d3c02
Nishim Goyal <nishimgoyal@Nishim.local>
2024-01-22 18:33:34 +0530

    wss

--- indexer.py
+++ indexer.py
@@ -1,9 +1,11 @@
 import os
-import requests
+import json
 from pymongo import MongoClient
+from websocket import create_connection
+import ssl
 import time
 
-url = 'https://solana-mainnet.g.alchemy.com/v2/CGiyOMQ5iorRtcPm2xnU7qOSkocN2Ps7'
+url = 'wss://solana-mainnet.g.alchemy.com/v2/CGiyOMQ5iorRtcPm2xnU7qOSkocN2Ps7'
 headers = {'Content-Type': 'application/json'}
 
 # Connect to MongoDB
@@ -21,8 +23,11 @@ if not os.path.exists(logs):
             "id": 1,
             "method": "getSlot"
         }
-        slot_response = requests.post(url, json=slot_data, headers=headers)
-        slot_number = slot_response.json()['result']
+        ws = create_connection(url, sslopt={"cert_reqs": ssl.CERT_NONE})
+        ws.send(json.dumps(slot_data))
+        response = ws.recv()
+        slot_number = json.loads(response)['result']
+        ws.close()
 else:
     with open(logs, "r") as file:
         data = file.read().split('\n')
@@ -30,16 +35,16 @@ else:
         restart_count = int(data[1])
 
 last_processed_block = 0
-islot=slot_number
-_itime=time.time()
-itime=None
-processing_time=None
-avg_speed=None
+islot = slot_number
+itime = None
+processing_time = None
+avg_speed = None
+
 try:
     while True:
         print(f"Indexing block: {slot_number}")
         # Fetch the block for the current slot
-        itime=time.time()
+        itime = time.time()
         block_data = {
             "jsonrpc": "2.0",
             "id": 1,
@@ -55,16 +60,19 @@ try:
             ]
         }
 
-        response = requests.post(url, json=block_data, headers=headers)
-        response_json = response.json()
+        ws = create_connection(url, sslopt={"cert_reqs": ssl.CERT_NONE})
+        ws.send(json.dumps(block_data))
+        response = ws.recv()
+        response_json = json.loads(response)
+        ws.close()
 
         for transaction in response_json['result']['transactions']:
             transaction['block'] = slot_number
             collection.insert_one(transaction)
 
         last_processed_block = slot_number
-        processing_time=time.time()-itime
-        avg_speed=(last_processed_block-islot)/(time.time()-_itime)
+        processing_time = time.time() - itime
+        avg_speed = (last_processed_block - islot) / (time.time() - itime)
         slot_number += 1
         with open(logs, "w") as file:
             file.write(f"{last_processed_block}\n{restart_count}\n{processing_time}\n{avg_speed}")
@@ -72,17 +80,12 @@ try:
 except Exception as e:
     print(f"An error occurred: {e}")
 
-
-
 finally:
     # Increment restart count
     restart_count += 1
 
     # Update api file with slot number and restart count
-    
     with open(logs, "w") as file:
         file.write(f"{last_processed_block}\n{restart_count}\n{processing_time}\n{avg_speed}")
 
     client.close()
-
-
